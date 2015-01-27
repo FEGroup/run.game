bbebe6df5f1129b74d9cb97ee7462dc7e0641847
Jinkwon <master@bdyne.net>
2015-01-27 12:24:59 +0900

    feature : hero Model works

--- app/scripts/model/run.Model.js
+++ app/scripts/model/run.Model.js
@@ -1,8 +1,21 @@
 run.Model = (function(){
 
   return run.Class.extend({
+
     initialize : function(){
 
+    },
+
+    get : function(prop){
+      if(this.hasOwnProperty(prop)){
+        return this[prop];
+      } else {
+        throw new Error('no such config value' + prop);
+      }
+    },
+
+    set : function(prop, val){
+      this[prop] = val;
     }
   });
 
